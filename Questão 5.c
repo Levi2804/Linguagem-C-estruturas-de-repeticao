e3770c3ba9469f54e56c5ade65235fa804e31c10
Levi Araújo <166538311+Levi2804@users.noreply.github.com>
2024-04-16 08:30:39 -0300

    Update and rename Questão 5.cpp to Questão 5.c

--- Questão 5.c
+++ Questão 5.c
@@ -7,23 +7,23 @@ int main(){
 	float n1, n2, n3, m;
 	for(i = 0; i <= 10; i++){
 		printf("=======================");
-		printf("\nDigite a 1ª nota: ");
+		printf("\nDigite a 1Âª nota: ");
 		scanf("%f", &n1);
-		printf("Digite a 2ª nota: ");
+		printf("Digite a 2Âª nota: ");
 		scanf("%f", &n2);
-		printf("Digite a 3ª nota: ");
+		printf("Digite a 3Âª nota: ");
 		scanf("%f", &n3);
 		m = (n1 + n2 + n3)/3;
 		if(m >= 9){
-			printf("\nMédia %.1f e conceito A\n", m);
+			printf("\nMÃ©dia %.1f e conceito A\n", m);
 		} else if(m >= 7 && m < 9){
-			printf("\nMédia %.1f e conceito B\n", m);
+			printf("\nMÃ©dia %.1f e conceito B\n", m);
 		} else if(m >= 6 && m < 7){
-			printf("\nMédia %.1f e conceito C\n", m);
+			printf("\nMÃ©dia %.1f e conceito C\n", m);
 		} else if(m >= 4 && m < 6){
-			printf("\nMédia %.1f e conceito D\n", m);
+			printf("\nMÃ©dia %.1f e conceito D\n", m);
 		} else{
-			printf("\nMédia %.1f e conceito E\n", m);
+			printf("\nMÃ©dia %.1f e conceito E\n", m);
 		}
 	}
 	return(0);
