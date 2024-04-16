89a176a5e95b179dc5e3242d440b2571fa49f89e
Levi Araújo <166538311+Levi2804@users.noreply.github.com>
2024-04-16 08:30:31 -0300

    Update and rename Questão 4.cpp to Questão 4.c

--- Questão 4.c
+++ Questão 4.c
@@ -4,7 +4,7 @@
 int main(){
 	setlocale(LC_ALL, "Portuguese");
 	int i, cont = 0;
-	printf("O total de números divisíveis por 2 existentes no intervalo de 10 a 50:\n\n");
+	printf("O total de nÃºmeros divisÃ­veis por 2 existentes no intervalo de 10 a 50:\n\n");
 	for(i = 10; i <= 50; i++){
 		if(i % 2 == 0){
 			printf("%i  ", i);
